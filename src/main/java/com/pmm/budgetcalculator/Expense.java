99bc684f88553f03c8e96b6ff683fe8b0593680d
ajcytrap <pmm.gwo@gmail.com>
2022-11-25 10:43:59 +0100

    "adding new class Expense"

---
+++ src/main/java/com/pmm/budgetcalculator/Expense.java
@@ -0,0 +1,63 @@
+package com.pmm.budgetcalculator;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Expense {
+    private String expenseName;
+    private String expenseDescription;
+    private LocalDateTime expenseTime;
+    private BigDecimal expenseAmount;
+    private String expensePlace;
+    private long expenseId;
+
+    public String getExpenseName() {
+        return expenseName;
+    }
+
+    public void setExpenseName(String expenseName) {
+        this.expenseName = expenseName;
+    }
+
+    public String getExpenseDescription() {
+        return expenseDescription;
+    }
+
+    public void setExpenseDescription(String expenseDescription) {
+        this.expenseDescription = expenseDescription;
+    }
+
+    public BigDecimal getExpenseAmount() {
+        return expenseAmount;
+    }
+
+    public void setExpenseAmount(BigDecimal expenseAmount) {
+        this.expenseAmount = expenseAmount;
+    }
+
+    public LocalDateTime getExpenseTime() {
+        return expenseTime;
+    }
+
+    public void setExpenseTime(LocalDateTime expenseTime) {
+        this.expenseTime = expenseTime;
+    }
+
+    public String getExpensePlace() {
+        return expensePlace;
+    }
+
+    public void setExpensePlace(String expensePlace) {
+        this.expensePlace = expensePlace;
+    }
+
+    public long getExpenseId() {
+        return expenseId;
+    }
+
+    public void setExpenseId(long expenseId) {
+        this.expenseId = expenseId;
+    }
+}
