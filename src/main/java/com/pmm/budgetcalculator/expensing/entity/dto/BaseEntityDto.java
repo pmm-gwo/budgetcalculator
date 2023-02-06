c57f36c3d507cdfbd9763fd0e30f55a4690b1e36
ajcytrap <pmm.gwo@gmail.com>
2023-02-06 17:35:35 +0100

    "Annotations: @Data @Builder @NoArgsConstructor @AllArgsConstructor"

--- src/main/java/com/pmm/budgetcalculator/expensing/entity/dto/BaseEntityDto.java
+++ src/main/java/com/pmm/budgetcalculator/expensing/entity/dto/BaseEntityDto.java
@@ -1,12 +1,18 @@
 package com.pmm.budgetcalculator.expensing.entity.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntityDto {
     @Id
     protected Long id;
--- src/main/java/com/pmm/budgetcalculator/expensing/entity/dto/ExpensePlanDto.java
+++ src/main/java/com/pmm/budgetcalculator/expensing/entity/dto/ExpensePlanDto.java
@@ -1,13 +1,19 @@
 package com.pmm.budgetcalculator.expensing.entity.dto;
 
 import com.pmm.budgetcalculator.expensing.entity.ExpenseCategory;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
-public class ExpensePlanDTO {
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpensePlanDto {
 
     private String expensePlanName;
     private String expensePlanDescription;
