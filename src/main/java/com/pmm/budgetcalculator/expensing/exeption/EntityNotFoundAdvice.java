c496991782d83c9381569ade716c99f6f0c7e088
ajcytrap <pmm.gwo@gmail.com>
2023-02-09 11:53:56 +0100

    "Exception Handler working"

--- src/main/java/com/pmm/budgetcalculator/expensing/exeption/EntityNotFoundAdvice.java
+++ src/main/java/com/pmm/budgetcalculator/expensing/exeption/EntityNotFoundAdvice.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import javax.persistence.EntityNotFoundException;
 
 @ControllerAdvice
 public class EntityNotFoundAdvice {
--- src/main/java/com/pmm/budgetcalculator/expensing/exeption/EntityNotFoundException.java
+++ src/main/java/com/pmm/budgetcalculator/expensing/exeption/EntityNotFoundException.java
@@ -2,6 +2,7 @@ package com.pmm.budgetcalculator.expensing.exeption;
 
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(Long id) {
+
         super("Entity on id " + id + " doesn't exist.");
     }
 }
